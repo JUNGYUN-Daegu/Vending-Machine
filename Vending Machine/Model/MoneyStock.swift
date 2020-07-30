b98b2a2d2364165e7d1337761616234445674d08
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2020-07-30 18:24:48 +0900

    temporary review

---
+++ Vending Machine/Model/MoneyStock.swift
@@ -0,0 +1,16 @@
+//
+//  MoneyStock.swift
+//  Vending Machine
+//
+//  Created by 조중윤 on 2020/07/30.
+//  Copyright © 2020 Jojung-yun. All rights reserved.
+//
+
+import Foundation
+
+struct MoneyStock {
+    
+    var initialMoney = InitialMoney(InitialMoney: 1000000)
+    
+    
+    }
