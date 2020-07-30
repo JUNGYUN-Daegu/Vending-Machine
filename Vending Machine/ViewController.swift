42c6f8070d9e387b08098b8ee4d93e2390f6a7f7
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2020-07-30 15:08:01 +0900

    Initial commit

---
+++ Vending Machine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vending Machine
+//
+//  Created by 조중윤 on 2020/07/30.
+//  Copyright © 2020 Jojung-yun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello")
+    }
+
+
+}
+
